b64d361578754203953a1dc0e93810124ecfbef4
korean119 <tnclswnsdh@gmail.com>
2023-12-26 22:52:28 +0900

    fix(app): 파일 분리 시킴

---
+++ src/core/api.ts
@@ -0,0 +1,31 @@
+import { NewsFeed, NewsDetail } from '../types';
+
+export class Api {
+  ajax: XMLHttpRequest;
+  url: string;
+
+  constructor(url: string) {
+    this.ajax = new XMLHttpRequest();
+    this.url = url;
+  }
+
+  getRequest<AjaxResponse>(): AjaxResponse {
+    const ajax = new XMLHttpRequest();
+    ajax.open('GET', this.url, false);
+    ajax.send();
+
+    return JSON.parse(ajax.response);
+  }
+}
+
+export class NewsFeedApi extends Api {
+  getData(): NewsFeed[] {
+    return this.getRequest<NewsFeed[]>();
+  }
+}
+
+export class NewsDetailApi extends Api {
+  getData(): NewsDetail {
+    return this.getRequest<NewsDetail>();
+  }
+}
---
+++ src/core/router.ts
@@ -0,0 +1,37 @@
+import { Routerinfo } from '../types';
+import View from '../core/view';
+
+export default class Router {
+  routeTable: Routerinfo[];
+  defaultRoute: Routerinfo | null;
+
+  constructor() {
+    window.addEventListener('hashchange', this.route.bind(this));
+
+    this.routeTable = [];
+    this.defaultRoute = null;
+  }
+
+  setDefaultPage(page: View): void {
+    this.defaultRoute = { path: '', page };
+  }
+
+  addRoutePath(path: string, page: View): void {
+    this.routeTable.push({ path, page });
+  }
+
+  route() {
+    const routerPath = location.hash;
+
+    if (routerPath === '' && this.defaultRoute) {
+      this.defaultRoute.page.render();
+    }
+
+    for (const routeInfo of this.routeTable) {
+      if (routerPath.indexOf(routeInfo.path) >= 0) {
+        routeInfo.page.render();
+        break;
+      }
+    }
+  }
+}
